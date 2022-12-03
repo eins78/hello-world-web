5dd06136784c03dc439a03344e04dee772de2746
Max F. Albrecht <1@178.is>
2022-12-03 17:59:51 +0100

    add /api/config

--- routes/api.js
+++ routes/api.js
@@ -3,6 +3,12 @@ const router = express.Router();
 const htmlTemplate = require("../views/html");
 const homeTemplate = require("../views/home");
 
+/* GET config */
+const config = require("../config");
+router.get("/config", function (req, res, next) {
+  res.json(config);
+});
+
 /* GET timestamp */
 router.get("/time", function (req, res, next) {
   const now = new Date();

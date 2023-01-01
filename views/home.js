3cbe4bbf7aac93e2d4022ad3a8d2e567c022888f
Max F. Albrecht <1@178.is>
2023-01-01 14:39:58 +0100

    improve client info data and view

--- views/home.js
+++ views/home.js
@@ -1,18 +1,36 @@
 // @ts-check
+
 const fs = require("fs");
 const path = require("path");
 const apiDocs = fs.readFileSync(path.join(__dirname, "./home/section-api.html"));
 
-module.exports = ({ title = "Title", config = {}, client = {} }) =>
-  `
+/**
+ * @param {{title?: string; config?: Record<string,string>, client?: import('../lib/client-info/clientInfo').getClientInfo | {}}} viewConfig
+ */
+module.exports = ({ title = "Title", config = {}, client = {} }) => {
+  const clientInfo = { ...client, headers: undefined, trailers: undefined };
+  const headersAndTrailers = { headers: client["headers"], trailers: client["trailers"] };
+
+  return `
   <h1>${title}</h1>
 
-  <h2>config</h2>
-  <pre>${JSON.stringify(config, 0, 2)}</pre>
+  <h2>info</h2>
 
-  <h2>client</h2>
-  <pre>${JSON.stringify(client, 0, 2)}</pre>
+  <details open><summary>config</summary>
+  <pre>${JSON.stringify(config, null, 2)}</pre>
+  </details>
+
+  <details>
+    <summary>client info</summary>
+    <pre>${JSON.stringify(clientInfo, null, 2)}</pre>
+
+    <details>
+      <summary>headers and trailers</summary>
+      <pre>${JSON.stringify(headersAndTrailers, null, 2)}</pre>
+    </details>
+  </details>
 
   ${apiDocs}
 
 `.trim();
+};

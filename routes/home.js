c89ffd9b42e560f6d386f153e670294cc9e94494
Max F. Albrecht <1@178.is>
2023-01-01 13:46:24 +0100

    view: add client info on homepage

--- routes/home.js
+++ routes/home.js
@@ -4,14 +4,16 @@ const router = express.Router();
 const htmlTemplate = require("../views/html");
 const homeTemplate = require("../views/home");
 const config = require("../config");
+const { getClientInfo } = require("../lib/client-info/clientInfo");
 const title = process.env.APP_TITLE ?? "Hello World!";
 
 /* GET home page. */
 router.get("/", function (req, res, next) {
-  const pageData = { title, config };
+  const client = getClientInfo(req);
+  const pageData = { title, config, client };
   res.send(
     htmlTemplate({
-      title,
+      htmlTitle: title,
       bodyContent: homeTemplate(pageData),
     })
   );
--- views/home.js
+++ views/home.js
@@ -3,12 +3,15 @@ const fs = require("fs");
 const path = require("path");
 const apiDocs = fs.readFileSync(path.join(__dirname, "./home/section-api.html"));
 
-module.exports = ({ title = "Title", config = null }) =>
+module.exports = ({ title = "Title", config = {}, client = {} }) =>
   `
   <h1>${title}</h1>
 
   <h2>config</h2>
   <pre>${JSON.stringify(config, 0, 2)}</pre>
+
+  <h2>client</h2>
+  <pre>${JSON.stringify(client, 0, 2)}</pre>
 
   ${apiDocs}
 

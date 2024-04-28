0f852e5e1be9115b2dfdc175e4a92fe96e4aec46
Max Albrecht <1@178.is>
2024-04-28 18:49:11 +0200

    add correct types for express app

--- app.js
+++ app.js
@@ -12,6 +12,7 @@ const { basePath } = config;
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
+/** @type {import('express').Express} */
 const app = express();
 
 app.use(logger("dev"));

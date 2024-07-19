bf2e4c0ea4768fb43e99614364ad591e1d5da8e6
Max Albrecht <1@178.is>
2024-07-19 20:24:21 +0200

    chore: add generated cypress setup and example spec

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,10 +1,17 @@
 // @ts-check
-
 import eslint from "@eslint/js";
 import tseslint from "typescript-eslint";
 import globals from "globals";
+import pluginCypress from "eslint-plugin-cypress";
+import pluginCypressFlat from "eslint-plugin-cypress/flat";
+import testingLibrary from "eslint-plugin-testing-library";
+import { fixupPluginRules } from "@eslint/compat";
 
 export default tseslint.config(
+  // recommended config
+  eslint.configs.recommended,
+
+  // nodejs
   {
     languageOptions: {
       globals: {
@@ -12,6 +19,24 @@ export default tseslint.config(
       },
     },
   },
-  eslint.configs.recommended,
+
+  // tests config
+  // - cypress
+  // - testing-library (not yet compatible with eslint flat config)
+  {
+    files: ["cypress/**/*.*s", "test/**/*.*s"],
+    ...pluginCypressFlat.configs.recommended,
+    plugins: {
+      cypress: fixupPluginRules(pluginCypress),
+      "testing-library": fixupPluginRules({
+        rules: testingLibrary.rules,
+      }),
+    },
+    rules: {
+      ...testingLibrary.configs.dom.rules,
+    },
+  },
+
+  // ignore generated files
   { ignores: ["views/lit-ssr-demo/lib"] }
 );

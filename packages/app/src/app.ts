300aca9afe1f70a9ad137a0d11a66693f14456bc
Max Albrecht <1@178.is>
2025-02-01 10:55:48 +0100

    fix: resolve correct path lit-ssr-demo client for static server

--- packages/app/src/app.ts
+++ packages/app/src/app.ts
@@ -3,6 +3,7 @@ import express, { json, urlencoded, type Express } from "express";
 import logger from "morgan";
 import { fileURLToPath } from "node:url";
 import path from "node:path";
+import { createRequire } from "node:module";
 
 import config from "./config.ts";
 import { apiRouter } from "./routes/api/index.ts";
@@ -12,6 +13,7 @@ import litSsrDemoRouter from "./routes/lit-ssr-demo.ts";
 const { basePath } = config;
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
+const require = createRequire(import.meta.url);
 
 const app: Express = express();
 
@@ -25,7 +27,7 @@ app.use(basePath, express.static(path.join(__dirname, "..", "public")));
 // demo static files
 app.use(
   path.join(basePath, "lit-ssr-demo"),
-  express.static(path.join(__dirname, "..", "..", "lit-ssr-demo", "lib", "client")),
+  express.static(path.dirname(require.resolve("@hello-world-web/lit-ssr-demo/client"))),
 );
 
 // app routes

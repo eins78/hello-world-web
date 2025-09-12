36cd6bde67cd6fde62271ae1edb480773887d010
Max Albrecht <1@178.is>
2025-09-12 22:50:37 +0200

    chore: add APP_DESCRIPTION environment variable to Docker run script

--- scripts/build-run-docker.sh
+++ scripts/build-run-docker.sh
@@ -7,6 +7,7 @@ cd "$(dirname "$0")/.."
 export IMG=hello-world-web
 export PORT=8080
 export "APP_TITLE=Hello ${USER}@$(hostname -s)"!
+export "APP_DESCRIPTION=This is the <code>hello-world-web</code> app running in a Docker container on <samp>$(uname -a)</samp>"
 
 time docker buildx build --load -t $IMG .
-docker run --rm -it -e APP_TITLE -e PORT -p $PORT:$PORT $IMG
+docker run --rm -it -e APP_TITLE -e APP_DESCRIPTION -e PORT -p $PORT:$PORT $IMG

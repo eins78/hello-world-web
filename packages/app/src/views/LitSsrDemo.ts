ecb6aea9fbb867398509842faa2bd9ca22b4e71a
Max Albrecht <1@178.is>
2025-10-24 21:23:54 +0200

    fix: correct relative path depth
    Fixed relative path from packages/app/src/views/ to
    packages/lit-ssr-demo/lib/ - needs ../../../ not ../../

--- packages/app/src/views/LitSsrDemo.ts
+++ packages/app/src/views/LitSsrDemo.ts
@@ -9,7 +9,7 @@ import {
   DataTableComponent,
   EpochCounterComponent,
   registerCustomElements,
-} from "../../lit-ssr-demo/lib/server/entry-server.js";
+} from "../../../lit-ssr-demo/lib/server/entry-server.js";
 import type { JSON, JsonObject } from "../support/json.js";
 import type { ServerTemplate } from "../support/render-view/renderView.js";
 
